ef823d2dbe9d6b34d219a658faa975c2012453f2
SubMishMar <subodh514@tamu.edu>
2021-05-31 12:38:38 -0500

    added a figure

--- linkalibr/data/plotVelocity.m
+++ linkalibr/data/plotVelocity.m
@@ -17,6 +17,7 @@ hold off;
 ylabel('v_x');
 grid;
 title('v_x time plot');
+legend('Estimate', '1 \sigma bound');
 subplot(312)
 plot(velocity(:,2), 'LineWidth', 3);
 hold on;
@@ -27,6 +28,7 @@ hold off;
 ylabel('v_y');
 grid;
 title('v_y time plot');
+legend('Estimate', '1 \sigma bound');
 subplot(313)
 plot(velocity(:,3), 'LineWidth', 3);
 hold on;
@@ -37,3 +39,4 @@ hold off;
 ylabel('v_z');
 grid;
 title('v_z time plot');
+legend('Estimate', '1 \sigma bound');
